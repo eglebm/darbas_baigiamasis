f65fa4d39818cc66a462ae77ae7418445334594b
eglebm <egle.budreviciute@gmail.com>
2021-12-21 02:22:52 +0200

    Section about in process

---
+++ script.js
@@ -0,0 +1,24 @@
+ //
+// About section tab navigation
+//
+
+const tabNavigationElements = document.querySelectorAll(".about .tab-navigation li");
+const tabContentElements = document.querySelectorAll(".about .tab-content");
+
+function removeActiveClassesFromElements(elements) {
+    for (let element of elements) {
+        element.classList.remove("active");
+    }
+}
+
+for (let tabElement of tabNavigationElements) {
+    tabElement.addEventListener("click", function() {
+        const target = this.dataset.target; // "register" / "apply" / "receive"
+
+        removeActiveClassesFromElements(tabNavigationElements);
+        this.classList.add("active");
+
+        removeActiveClassesFromElements(tabContentElements);
+        document.querySelector('.tab-content[data-tab="'+target+'"]').classList.add('active');
+    });
+}
